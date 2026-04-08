c765d64c85346117ea068942c5435b65992bac80
Aaron J <jamesacs329@gmail.com>
2026-04-08 13:57:56 -0400

    add make an api demo

---
+++ make-an-api/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+import random
+import string
+
+app = FastAPI()
+data = {}
+
+
+@app.post("/ip/{ip}")
+def create_code(ip: str):
+    # Generate a simple 4-letter code
+    code = "".join(random.choices(string.ascii_lowercase, k=5))
+
+    data[code] = ip
+    return {"code": code, "ip": ip}
+
+
+@app.get("/code/{code}")
+def get_ip(code: str):
+    if code not in data:
+        raise HTTPException(status_code=404, detail="Code not found")
+    return {"ip": data.get(code, "Not found")}
